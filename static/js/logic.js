391cba25957900e18b307cbf58c5b6b16e51b43e
Leah Stucky <l.stucky406@gmail.com>
2020-07-21 22:41:19 -0600

    final

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,10 +1,12 @@
 
-
+// GeoJSON url
 var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
+// API Call
 d3.json(baseURL, function(earthquakeData) {
     console.log(earthquakeData);
 
+    //Create tile layers
     var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",     
         tileSize: 512,
@@ -23,13 +25,17 @@ d3.json(baseURL, function(earthquakeData) {
         accessToken: API_KEY
     });
 
+    // Define tile layers as base maps
     var baseMaps = {
         Satellite: satellite,
         Grayscale: grayscale
     };
-        
+    
+    // Create GeoJSON layer with earthquake bubble markers 
     var earthquakes = L.geoJSON(earthquakeData, {
         pointToLayer: function(feature, latlng) {
+
+            // Set color based on magnitude size
             var color = "";
             if (feature.properties.mag > 5) {
                 color = "#ff0400";
@@ -44,6 +50,7 @@ d3.json(baseURL, function(earthquakeData) {
             } else {
                 color = "#bfff00";
             };
+            // Set bubble marker radius on magnitude size
             var markers = {
                 radius: feature.properties.mag * 3,
                 fillOpacity: 0.75,
@@ -51,16 +58,18 @@ d3.json(baseURL, function(earthquakeData) {
                 fillColor: color,
                 weight: 0.5
             };
+            // Add popup with extra data
             return L.circleMarker(latlng, markers)
                 .bindPopup(`<h3>${feature.properties.place}</h3><hr/><p>Magnitude:${feature.properties.mag}</p><p>Time:${feature.properties.time}</p>`)  
         }
     });
 
+    // Define earthquake markers as overlay map
     var overLayMaps = {
-        // FaultLines: faultLines,
         Earthquakes: earthquakes
     };
 
+    // Create map with initial layers
     var myMap = L.map('map', {
         center: [39.8283, -98.5795],
         zoom: 4,
@@ -68,25 +77,43 @@ d3.json(baseURL, function(earthquakeData) {
     });
 
 
-    //Legend NOT WORKING
-    var legend = L.control({ position:'bottomright'});
-    legend.onAdd = function (map) {
-        var div = L.DomUtil.create('div', 'info legend')
-
-        var legendInfo = {
-            legendColors: ["#bfff00", "#eeff00", "#ffd900", "#ff9100", "#ff5900", "#ff0400"],
-            legendLabels: ['0-1', '1-2', '2-3', '3-4', '4-5', '5+']
-        }
-
-        for (i = 0; i < legendInfo; i++) {
-            div.innerHTML = `<div><h3>Magnitude</h3><br></div><div class="labels"><li style="background-color:${legendColors[i]}"></li></div>`
-        }
-      return div
+    // Create Legend ** not showing on map **
+    // Create function to get colors for magnitude scale
+    function getColor(d) {
+        return d > 5 ? "#ff0400" :
+               d > 4 ? "#ff5900" :
+               d > 3 ? "#ff9100" :
+               d > 2 ? "#ffd900" :
+               d > 1 ? "#eeff00" :
+               "#bfff00";                     
     }
 
-    L.control.layers(baseMaps, overLayMaps, legend).addTo(myMap);
+    // set legend control on bottom left
+    var legend = L.control({position: 'bottomleft'});
+
+    // Create div element with 'info legend' class
+    legend.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'info legend'),
+            scale = [0, 1, 2, 3, 4, 5]
+            labels = [];
+    
+        // loop through our scale intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < scale.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(scale[i] + 1) + '"></i> ' +
+                scale[i] + (scale[i + 1] ? '&ndash;' + scale[i + 1] + '<br>' : '+');
+        }
+        div.innerHTML = labels.join('<br>');
+        return div;
+    };
+
+    // Create layer control and pass in basemaps, overlaymap, and legend to map
+    L.control.layers(baseMaps, overLayMaps, legend, {
+        collapsed: false
+    }).addTo(myMap);
 
   });
-  
 
 
+    
+    
